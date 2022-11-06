45282da8e910d21cb51e1953d3d73bc41a4534ed
Roman Pelaez <e.roman@ibermatica.com>
2022-11-06 23:28:00 +0100

    mis primeros tests

--- cypress/integration/sifthing_content.spec.js
+++ cypress/integration/sifthing_content.spec.js
@@ -5,7 +5,7 @@ it('A page contains 5 elements', ()=>{
 cy.visit('https://the-internet.herokuapp.com/')
 cy.get(':nth-child(39) > a').click()
 cy.get('[href="/shifting_content/menu"]').click()
-cy.get('#content').children().should('have.length', 5)
+cy.get('li').should('have.length', 5)
 
 
 
