b16c111906f7f94c73c8c181db6aa77b43c239ba
Roman Pelaez <e.roman@ibermatica.com>
2022-11-07 19:18:30 +0100

    before each

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,59 +1,52 @@
 /// <reference types="cypress" />
 
-it('A valid user can login', () =>{
-cy.visit('https://the-internet.herokuapp.com/')
-cy.get(':nth-child(21) > a').click()
-cy.get('#username').type('tomsmith')
-cy.get('#password').type('SuperSecretPassword!')
-cy.get('.fa').click()
-cy.get('#flash').contains('You logged into a secure area!')
-})
+describe('test de login', () => { 
+ beforeEach(() => {
+  cy.visit('https://the-internet.herokuapp.com/') 
+  cy.get(':nth-child(21) > a').click()
+  
+ })
+ 
+   it('A valid user can login', () =>{
+
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('.fa').click()
+    cy.get('#flash').contains('You logged into a secure area!')
+   })
 
 
-it('An invalid password cannot login', () =>{
-
-    cy.visit('https://the-internet.herokuapp.com/')
-    cy.get(':nth-child(21) > a').click()
+   it('An invalid password cannot login', () =>{
+  
     cy.get('#username').type("tomsmith")
     cy.get('#password').type("error!")
     cy.get('.fa').click()
     cy.get('#flash').contains("Your password is invalid!")
-       })
+   })
 
-it('An invalid username cannot login', () =>{
+   it('An invalid username cannot login', () =>{
+   
+    cy.get('#username').type('error!')
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('.fa').click()
+    cy.get('#flash').contains('Your username is invalid!')
+   })
 
-        cy.visit('https://the-internet.herokuapp.com/')
-        cy.get(':nth-child(21) > a').click()
-        cy.get('#username').type('error!')
-        cy.get('#password').type('SuperSecretPassword!')
-        cy.get('.fa').click()
-        cy.get('#flash').contains('Your username is invalid!')
-      
-      
-      
-      
-         })
+   it('Blank information cannot login', () =>{
 
-it('Blank information cannot login', () =>{
+    cy.get('.fa').click()
+    cy.get('#flash').contains('Your username is invalid!')
+                
+   })
 
-            cy.visit('https://the-internet.herokuapp.com/')
-            cy.get(':nth-child(21) > a').click()
-            cy.get('.fa').click()
-            cy.get('#flash').contains('Your username is invalid!')
-          
-          
+   it ('A empty user and password cannot login', () =>{
 
-          
-             })
-
-it ('A empty user and password cannot login', () =>{
-                cy.visit("https://the-internet.herokuapp.com/")
-                cy.get(':nth-child(21) > a').click()
-                cy.get('#username').clear()
-                cy.get('#password').clear()
-                cy.get('.fa').click()
-                cy.get('#flash').contains("Your username is invalid!")
+    cy.get('#username').clear()
+    cy.get('#password').clear()
+    cy.get('.fa').click()
+    cy.get('#flash').contains("Your username is invalid!")
             
-                })             
+    })             
 
 
+})
